e23aea6714ac3756af29e7684694258238fb3a54
jiwon <jidole041214@naver.com>
2022-03-14 16:48:19 +0900

    add multer

--- routes/missing.js
+++ routes/missing.js
@@ -1,7 +1,38 @@
 const express = require("express");
 const MissingPerson = require("../schemas/missingPerson");
-
+const multer = require("multer");
+const fs = require("fs");
 const router = express.Router();
+const path = require("path");
+
+// 업로드 폴더 구축
+try {
+  fs.readdirSync("uploads");
+} catch (error) {
+  console.error("uploads 폴더가 없어 uploads 폴더를 생성합니다.");
+  fs.mkdirSync("uploads");
+}
+
+// multer init
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, cb) {
+      cb(null, "uploads/");
+    },
+    filename(req, file, cb) {
+      const ext = path.extname(file.originalname);
+      cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
+    },
+  }),
+  limits: { fileSize: 5 * 1024 * 1024 },
+});
+
+// get image url
+const type = upload.single("file");
+router.post("/upload/img", type, (req, res) => {
+  console.log(req.file);
+  res.json({ url: `/img/${req.file.filename}` });
+});
 
 router.get("/all", async (req, res, next) => {
   try {
